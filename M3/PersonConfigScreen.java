420cc2e0229c1b15027eea430349653c989bbdf5
bmawji3 <bmawji3@gatech.edu>
2015-09-09 21:14:20 -0400

    added PersonConfigScreen Files

---
+++ M3/PersonConfigScreen.java
@@ -0,0 +1,24 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+// import javafx.scene.control.ComboBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class PersonConfigScreen extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root =
+            FXMLLoader.load(getClass().getResource("PersonConfigScreen.fxml"));
+
+        Scene scene = new Scene(root, 1600, 900);
+
+        stage.setTitle("Person Configuration Screen");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
