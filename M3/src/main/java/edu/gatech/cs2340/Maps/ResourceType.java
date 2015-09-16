218dd45556c9155c6f61c4f4843936c56e1333e1
bmawji3 <bmawji3@gatech.edu>
2015-09-16 13:33:52 -0400

    Added ResourceType.java and Tile.java

---
+++ M3/src/main/java/edu/gatech/cs2340/Maps/ResourceType.java
@@ -0,0 +1,9 @@
+package edu.gatech.cs2340.Maps;
+
+/**
+ * @author Bilal
+ * @version 1.0
+ */
+public enum ResourceType {
+    RIVER, PLAIN, MOUNTAIN_1, MOUNTAIN_2, MOUNTAIN_3;
+}
---
+++ M3/src/main/java/edu/gatech/cs2340/Maps/Tile.java
@@ -0,0 +1,14 @@
+package edu.gatech.cs2340.Maps;
+
+import edu.gatech.cs2340.players.Person;
+
+public abstract class Tile {
+    private String color;
+    private Person owner;
+    private ResourceType resource;
+    private int food;
+    private int energy;
+    private int ore;
+    private int crystite;
+
+}
