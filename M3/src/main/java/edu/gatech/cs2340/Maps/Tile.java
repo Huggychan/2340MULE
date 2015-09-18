850eb487661900ae79bcd14a6dc4d90418bfa54f
bmawji3 <bmawji3@gatech.edu>
2015-09-18 13:54:28 -0400

    added javadoc

--- M3/src/main/java/edu/gatech/cs2340/Maps/Tile.java
+++ M3/src/main/java/edu/gatech/cs2340/Maps/Tile.java
@@ -3,6 +3,11 @@ package edu.gatech.cs2340.Maps;
 import edu.gatech.cs2340.GameObject.Mule;
 import edu.gatech.cs2340.players.Person;
 
+/**
+ * Tile class
+ * @author Bilal
+ * @version 1.0
+ */
 public class Tile {
     private String color;
     private Person owner;
@@ -15,74 +20,151 @@ public class Tile {
     private int yLoc;
     private Mule mule;
 
+    /**
+     * Tile construcot
+     * @param owner owner of tile
+     * @param resource resource of tile
+     * @param mule mule set at that tile
+     * @param xLoc x position of tile
+     * @param yLoc y position of tile
+     */
+    public Tile(Person owner, TileType resource, Mule mule,
+                int xLoc, int yLoc) {
+        this.resource = resource;
+        this.owner = owner;
+        this.color = owner.getColor();
+        this.mule = null;
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+    }
+
+    /**
+     * @return x position
+     */
     public int getxLoc() {
         return xLoc;
     }
 
+    /**
+     * @param xLoc set x position to xLoc
+     */
     public void setxLoc(int xLoc) {
         this.xLoc = xLoc;
     }
 
+    /**
+     * @return y position
+     */
     public int getyLoc() {
         return yLoc;
     }
 
+    /**
+     * @param yLoc set y position to yLoc
+     */
     public void setyLoc(int yLoc) {
         this.yLoc = yLoc;
     }
 
+    /**
+     * @return String form of color
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * @param color color to be set
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * Person returned
+     * @return person owner of tile
+     */
     public Person getOwner() {
         return owner;
     }
 
+    /**
+     * @param owner set owner to this owner
+     */
     public void setOwner(Person owner) {
         this.owner = owner;
     }
 
-    public TileType getResource() {return resource;}
+    /**
+     * @return resource of tile
+     */
+    public TileType getResource() { return resource; }
 
-    public Mule getMule() {return mule;}
-
-    public void setMule(Mule mule) {this.mule = mule;}
-
+    /**
+     * @param resource set resource of tile
+     */
     public void setResource(TileType resource) {
         this.resource = resource;
     }
 
+    /**
+     * @return get Mule of tile
+     */
+    public Mule getMule() { return mule; }
+
+    /**
+     * @param mule set mule of tile
+     */
+    public void setMule(Mule mule) { this.mule = mule; }
+
+    /**
+     * @return food count
+     */
     public int getFood() {
         return food;
     }
 
+    /**
+     * @param food set food amount
+     */
     public void setFood(int food) {
         this.food = food;
     }
 
+    /**
+     * @return energy amount
+     */
     public int getEnergy() {
         return energy;
     }
 
+    /**
+     * @param energy set energy to tile
+     */
     public void setEnergy(int energy) {
         this.energy = energy;
     }
 
-    public int getOre() {
-        return ore;
-    }
+    /**
+     * @return ore of tile
+     */
+    public int getOre() { return ore; }
 
-    public void setOre(int ore) {this.ore = ore;}
+    /**
+     * @param ore set ore amount to tile
+     */
+    public void setOre(int ore) { this.ore = ore; }
 
+    /**
+     * @return crystite of tile
+     */
     public int getCrystite() {
         return crystite;
     }
 
+    /**
+     * @param crystite set crystite of tile
+     */
     public void setCrystite(int crystite) {
         this.crystite = crystite;
     }
