14be406e0589d094b225005b9f59050ce8044cfb
bmawji3 <bmawji3@gatech.edu>
2015-09-21 11:57:20 -0400

    changed placeholder to summary and added controller

---
+++ M3/src/main/java/edu/gatech/cs2340/configs/SummaryController.java
@@ -0,0 +1,22 @@
+package edu.gatech.cs2340.configs;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
+/**
+ * @author Bilal
+ * @version 1.0
+ */
+public class SummaryController {
+    @FXML
+    private HBox first;
+    @FXML
+    private HBox second;
+    @FXML
+    private HBox third;
+    @FXML
+    private HBox fourth;
+    @FXML
+    private HBox summary;
+
+}
