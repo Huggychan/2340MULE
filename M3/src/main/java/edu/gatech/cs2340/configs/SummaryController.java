5efdafc3eef00a70527549241f2431ee58de5353
bmawji3 <bmawji3@gatech.edu>
2015-09-21 20:04:00 -0400

    Working with SummaryController

--- M3/src/main/java/edu/gatech/cs2340/configs/SummaryController.java
+++ M3/src/main/java/edu/gatech/cs2340/configs/SummaryController.java
@@ -7,10 +7,13 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,7 +21,7 @@ import java.util.ResourceBundle;
  * @author Bilal
  * @version 1.0
  */
-public class SummaryController {
+public class SummaryController implements Initializable {
     @FXML
     private HBox first;
     @FXML
@@ -50,6 +53,10 @@ public class SummaryController {
 
 
     private Game game;
+    private Image firstPlace;
+    private Image secondPlace;
+    private Image thirdPlace;
+    private Image fourthPlace;
 
     /**
      * Initializes the fxml file
@@ -57,6 +64,8 @@ public class SummaryController {
      * @param resources Resources needed
      */
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        firstPlace = new Image("resources/MULE_Humanoid");
+        firstImage.setImage(firstPlace);
 //        firstText.setText(game.getPlayers().get(0).toString());
 //        secondText.setText(game.getPlayers().get(1).toString());
 //        thirdText.setText(game.getPlayers().get(2).toString());
